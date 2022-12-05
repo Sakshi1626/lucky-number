138faca3e927d4509fb65584cb520fa06b7a5a6e
sakshi <sakshigupta1603@gmail.com>
2022-12-05 23:24:49 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# lucky-number
+# lucky-birthday
+A website which can help you to check whether your birthday is lucky or not. You Have to Just Enter your DOB and Your Lucky Number.
+
+
+
+## [LIVE](https://lucky-number-nk.netlify.app/)
+
+---
+
+![Demo](/icons/app-ss-org.png)
+
+---
+<br/>
+<br/>
+
+# Technology
+
+- HTML
+- CSS
+- JavaScript
+- Hosted (Netlify)
  
---
+++ app.js
@@ -0,0 +1,36 @@
+var dob = document.querySelector("#dob");
+var luckyNumber = document.querySelector("#luckyNumber");
+var outPut = document.querySelector("#outPut");
+var button = document.querySelector("button");
+
+button.addEventListener("click", calculateLuckyBirthday);
+
+
+function calculateLuckyBirthday() {
+
+    var dobValue = dob.value;
+    dobValue = dobValue.replaceAll("-", "");
+    var sum = 0;
+
+
+    if (dobValue == 0 || luckyNumber.value.length == 0) {
+        outPut.innerHTML = "Please enter both the fields";
+        outPut.style.color = "red";
+    } else if (luckyNumber.value < 0) {
+        outPut.innerHTML = "Please Enter a valid number";
+        outPut.style.color = "#dc2626";
+    } else {
+        for (let i = 0; i < 8; i++) {
+            sum = sum + (dobValue % 10);
+            dobValue = ~~(dobValue / 10);
+        }
+
+        if (sum % luckyNumber.value == 0) {
+            outPut.innerHTML = "Congrats 💐! Your Birthdate is Lucky 🎉🥳";
+            outPut.style.color = "green";
+        } else {
+            outPut.innerHTML = "Sad 😔! " + luckyNumber.value + " is not Lucky Number 😕";
+            outPut.style.color = "#f97316";
+        }
+    }
+}
